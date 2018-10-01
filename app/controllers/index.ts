18136ca79578a8ce4605597c8a90e3572c985999
Anh Phuc <phucinfortech18@gmail.com>
2018-10-01 12:53:36 +0700

    Gần hoàn thiện

--- app/controllers/index.ts
+++ app/controllers/index.ts
@@ -67,9 +67,45 @@ $(document).ready(function() {
             console.log(thongTinDatVe.MangGheDuocDat);
 
         }
+        LoadThongTinDatVe(thongTinDatVe.MangGheDuocDat);
     })
 
+    //Load thong tin dat ghe
+    function LoadThongTinDatVe(mangGhe:Ghe[]){
+        let noiDung = ``;
+        for(let ghe of mangGhe){
+            noiDung +=`
+                <span>Ghế Số: ${ghe.SoGhe} - ${ghe.Gia}</span>
+                <span class='btnHuy' soghe='${ghe.SoGhe}' style='cursor:pointer;color:red'>Hủy</span>
+                <br />
+            `
+        }
+        $('#tongtindatve').html(noiDung);
+    }
+    $('body').delegate('.btnHuy','click',function(){
+        let soGhe:any = $(this).attr('soghe');
+        
+        thongTinDatVe.XoaGhe(soGhe);
+        LoadThongTinDatVe(thongTinDatVe.MangGheDuocDat);
 
+        $('.gheDaDat').each(function(index,btnGhe){
+            //trả về ds các button có class gheDaDat
+            console.log(btnGhe);
+            //số ghế trong mãng dsghe
+            let soGheBtnGhe = $(this).attr('soghe');
+            if(soGheBtnGhe == soGhe){
+                $(this).removeClass('gheDaDat');
+            }
+        })
+        // for(let i = 0; i < dsGhe.MangDanhSachGhe.length; i++ ){
+        //     let gheDaChon = dsGhe.MangDanhSachGhe[i].SoGhe;
+        //     //console.log(gheDaChon);
+        //     if(gheDaChon == soGhe){
+        //         $(this).removeClass('gheDaDat');
+        //     }
+
+        // }
+    })
 
 
 
